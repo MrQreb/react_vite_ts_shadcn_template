6b6229c364c136d31e93ec240b41e75cb4876f6b
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2026-05-27 08:43:36 -0600

    fix

--- src/features/auth/routes/routes.ts
+++ src/features/auth/routes/routes.ts
@@ -1,10 +1,10 @@
 import { createRoute } from '@tanstack/react-router'
-import LoginPage from '../login/pages/LoginPage'
 import { rootRoute } from '@/app/router/__root'
+import { ChatPage } from '@/features/chatbot/chat/pages/ChatPage';
 
 /** Routa del login */
 export const loginRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/',
-  component: LoginPage, 
+  component: ChatPage, 
 })
